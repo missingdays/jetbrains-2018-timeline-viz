629192511bf8fd3b277ec180970bac53d03b0ec7
Evgeny Bovykin <daysaremissing@gmail.com>
2018-04-21 21:27:14 +0300

    Visual improvements

--- chart.js
+++ chart.js
@@ -2,13 +2,18 @@
 DEFAULT_OPTIONS = {
   width: 600,
   height: 480,
+
   marginTop: 30,
   marginBottom: 30,
   marginRight: 20,
-  marginLeft: 50
-}
+  marginLeft: 50,
 
-var parseDate = d3.timeParse("%Y-%m-%d");
+  dotRadius: 3,
+  dotBigRadius: 4,
+  defaultDotFill: "#2B303A",
+  maxDotFill: "#BF211E",
+  minDotFill: "#92DCE5"
+}
 
 class Chart {
   constructor(name, data, options){
@@ -24,62 +29,80 @@ class Chart {
 
   initSvg(){
     this.svg = d3.select("#container")
-        .append("svg")
-            .attr("width", this.options.width + this.options.marginLeft + this.options.marginRight)
-            .attr("height", this.options.height + this.options.marginTop + this.options.marginBottom)
-            .attr("id", "#" + this.name)
-        .append("g")
-            .attr("transform", "translate(" + this.options.marginLeft + "," + this.options.marginTop + ")");
+      .append("svg")
+        .attr("width", this.options.width + this.options.marginLeft + this.options.marginRight)
+        .attr("height", this.options.height + this.options.marginTop + this.options.marginBottom)
+        .attr("id", "#" + this.name)
+      .append("g")
+        .attr("transform", "translate(" + this.options.marginLeft + "," + this.options.marginTop + ")");
   }
 
 
   plot(){
+    var self = this;
+
     // Set the ranges
     var x = d3.scaleTime().range([0, this.options.width]);
     var y = d3.scaleLinear().range([this.options.height, 0]);
 
-    // Define the axes
-    var xAxis = d3.axisBottom().scale(x).ticks(5);
+    var minY = d3.min(this.data, function(d){ return d.value; });
+    var maxY = d3.max(this.data, function(d) { return d.value; });
 
-    var yAxis = d3.axisLeft().scale(y).ticks(5);
+    minY *= 1.1;
+    maxY *= 1.1;
+
+    // Define the axes
+    var xAxis = d3.axisBottom().scale(x).ticks(10);
+
+    var yAxis = d3.axisLeft().scale(y).ticks(10);
 
     // Define the line
     var valueline = d3.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.value); });
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.value); });
 
     // Scale the range of the data
     x.domain(d3.extent(this.data, function(d) { return d.date; }));
-    y.domain([0, d3.max(this.data, function(d) { return d.value; })]);
+    y.domain([minY, maxY]);
 
     // Add the valueline path.
     this.svg.append("path")
-        .attr("class", "line")
-        .attr("d", valueline(this.data));
+      .attr("class", "line")
+      .attr("d", valueline(this.data));
         //.attr('stroke-width', 2)
         //.attr('stroke', 'black');
 
     // Add the scatterplot
-    this.svg.selectAll("dot")
+    this.svg.selectAll(".dot")
         .data(this.data)
       .enter().append("circle")
-        .attr("r", 6)
+        .attr("class", "dot")
+        .attr("id", function(d, i){ return self.getIdFor(d); })
+        .attr("r", this.options.dotRadius)
+        .attr("cx", function(d) { return x(d.date); })
+        .attr("cy", function(d) { return y(d.value); });
+
+    this.svg.selectAll(".dotMouse")
+        .data(this.data)
+      .enter().append("circle")
+        .attr("class", "dotMouse")
+        .attr("r", this.options.dotRadius * 2)
+        .attr("opacity", 0)
         .attr("cx", function(d) { return x(d.date); })
         .attr("cy", function(d) { return y(d.value); })
-        .attr("id", function(d, i){ return "point" + i; })
         .on("mouseover", this.handleMouseOver())
         .on("mouseout", this.handleMouseOut());
 
     // Add the X Axis
     this.svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + this.options.height + ")")
-        .call(xAxis);
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + this.options.height + ")")
+      .call(xAxis);
 
     // Add the Y Axis
     this.svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis);
+      .attr("class", "y axis")
+      .call(yAxis);
   }
 
   handleMouseOver(){
@@ -87,77 +110,69 @@ class Chart {
 
     return function(d, i){
       var week = self.getWeekFor(d);
-      var mins = self.getMins(week);
-      var maxs = self.getMaxs(week);
+      var mins = self.getExtremums(week, 'min');
+      var maxs = self.getExtremums(week, 'max');
 
       week.forEach(function(day){
         if(mins.includes(day)){
-          d3.select('#point' + day).transition(300).attr('fill', 'orange');
+          self.svg.select("#" + self.getIdFor(day))
+            .attr("r", self.options.dotBigRadius)
+            .style('fill', self.options.minDotFill);
         } else if(maxs.includes(day)){
-          d3.select('#point' + day).transition(300).attr('fill', 'red')
+          self.svg.select("#" + self.getIdFor(day))
+            .attr("r", self.options.dotBigRadius)
+            .style('fill', self.options.maxDotFill);
         }
       });
 
       self.svg.append('rect')
           .attr('class', 'leftRectangle')
           .attr('x', 0)
-          .attr('y', -7)
-          .attr('width', d3.select('#point' + week[0]).attr('cx') - 5)
-          .attr('height', self.options.height + 7)
+          .attr('y', -self.options.dotRadius)
+          .attr('width', Math.max(self.svg  .select("#" + self.getIdFor(week[0])).attr('cx') - self.options.dotRadius, 0))
+          .attr('height', self.options.height + self.options.dotRadius)
           .attr('fill', 'black')
           .attr('opacity', 0)
-          .transition()
-          .duration(300)
           .attr('opacity', 0.5);
 
       self.svg.append('rect')
           .attr('class', 'rightRectangle')
-          .attr('x', parseInt(d3.select('#point' + week[week.length-1]).attr('cx')) + 5)
-          .attr('y', -7)
-          .attr('width', self.options.width - d3.select('#point' + week[week.length-1]).attr('cx'))
-          .attr('height', self.options.height + 7)
+          .attr('x', parseInt(self.svg.select("#" + self.getIdFor(week[week.length-1])).attr('cx')) + self.options.dotRadius)
+          .attr('y', -self.options.dotRadius)
+          .attr('width', self.options.width - self.svg.select("#" + self.getIdFor(week[week.length-1])).attr('cx'))
+          .attr('height', self.options.height + self.options.dotRadius)
           .attr('fill', 'black')
           .attr('opacity', 0)
-          .transition()
-          .duration(300)
           .attr('opacity', 0.5);
     }
   }
 
-  getMins(week){
-    var mins = [];
-    var min = Infinity;
+  getExtremums(data, type){
+    var extremumValue;
+    var cmp;
+
+    if(type == 'min'){
+      extremumValue = Infinity;
+      cmp = (a, b) => a < b;
+    } else {
+      extremumValue = -Infinity;
+      cmp = (a, b) => a > b;
+    }
+
+    var extremums = [];
     var self = this;
 
-    week.forEach(function(day){
-      if(self.data[day].value < min){
-        min = self.data[day].value;
-        mins = [day];
-      } else if(self.data[day].value == min){
-        mins.append(day);
+    data.forEach(function(day){
+      if(cmp(day.value, extremumValue)){
+        extremumValue = day.value;
+        extremums = [day];
+      } else if(day.value == extremumValue){
+        extremums.push(day);
       }
     });
 
-    return mins;
+    return extremums;
   }
-
-  getMaxs(week){
-      var mins = [];
-      var min = 0;
-      var self = this;
-
-      week.forEach(function(day){
-        if(self.data[day].value > min){
-          min = self.data[day].value;
-          mins = [day];
-        } else if(self.data[day].value == min){
-          mins.push(day);
-        }
-      });
-
-      return mins;
-    }
-
 
   handleMouseOut(){
     var self = this;
@@ -170,30 +185,33 @@ class Chart {
   clearSelection(){
     var self = this;
 
-    setTimeout(function(){
-      self.data.forEach(function(day, i){
-        d3.select('#point' + i).transition(300).attr('fill', 'black');
-      });
+    self.svg.selectAll(".dot")
+      .style('fill', self.options.defaultDotFill)
+      .attr("r", self.options.dotRadius);
 
-      d3.selectAll('.leftRectangle').transition(300).attr('opacity', 0).remove();
-      d3.selectAll('.rightRectangle').transition(300).attr('opacity', 0).remove();
-    }, 300);
+    self.svg.selectAll('.leftRectangle').remove();
+    self.svg.selectAll('.rightRectangle').remove();
   }
 
   getWeekFor(d){
     var week = [];
     var self = this;
 
-    this.data.forEach(function(day, i){
+    this.data.forEach(function(day){
       if(self.sameWeek(d.date, day.date)){
-        week.push(i);
+        week.push(day);
       }
     });
 
     return week;
   }
 
+
   sameWeek(d1, d2){
     return moment(d1).format('W') == moment(d2).format('W');
+  }
+
+  getIdFor(day){
+    return "point" + self.name + day.date.toISOString().slice(0,10);
   }
 }
--- example.js
+++ example.js
@@ -8,6 +8,21 @@ for(var i = 1; i < 32; i++){
   });
 }
 
+var handcraftedValues = [1, 1, 1, 4, 5, 5, 6,
+                         7, 8, 1, 2, 3, 1, 1,
+                         -4, -5, -6, -4, -5, -6, -5,
+                         1, 1, 1, 1, 1, 1, 1]
+var handcraftedData = [];
 
-var chart = new Chart('main', januaryData);
-chart.plot();
+for(var i = 1; i < 29; i++){
+  handcraftedData.push({
+    date: new Date(2018, 4, i),
+    value: handcraftedValues[i-1]
+  });
+}
+
+var handcraftedChart = new Chart('main', handcraftedData);
+handcraftedChart.plot();
+
+var randomChart = new Chart('random', januaryData);
+randomChart.plot();
