2445d047f63fb14cd5cdf5d7eb8737b33003b191
Evgeny Bovykin <daysaremissing@gmail.com>
2018-04-24 22:32:22 +0300

    Refactor. var -> let, const. function -> arrow function where convenient

--- chart.js
+++ chart.js
@@ -1,5 +1,5 @@
 
-DEFAULT_OPTIONS = {
+const DEFAULT_OPTIONS = {
   width: 600,
   height: 480,
 
@@ -36,33 +36,31 @@ class Chart {
   }
 
   plot(){
-    var self = this;
-
     this.svg.select('g').remove();
     this.canvas = this.svg.append('g')
         .attr('transform', `translate(${this.options.marginLeft}, ${this.options.marginTop})`)
 
-    var x = d3.scaleTime().range([0, this.options.width]);
-    var y = d3.scaleLinear().range([this.options.height, 0]);
+    const x = d3.scaleTime().range([0, this.options.width]);
+    const y = d3.scaleLinear().range([this.options.height, 0]);
 
-    var minY = d3.min(this.data, function(d){ return d.value; });
-    var maxY = d3.max(this.data, function(d) { return d.value; });
+    let minY = d3.min(this.data, d => d.value);
+    let maxY = d3.max(this.data, d => d.value);
 
     // Scales y axis such that min value isn't placed right on the x axis
     minY -= (maxY - minY) * 0.05;
     maxY += (maxY - minY) * 0.05;
 
     // Display every monday and the first day of the month. This way the month name is displayed.
-    var mondays = this.data.filter(day => day.date.getDay() == 1).map(day => day.date);
-    var xAxis = d3.axisBottom().scale(x).tickValues([this.data[0].date].concat(mondays));
+    const mondays = this.data.filter(day => day.date.getDay() == 1).map(day => day.date);
+    const xAxis = d3.axisBottom().scale(x).tickValues([this.data[0].date].concat(mondays));
 
-    var yAxis = d3.axisLeft().scale(y).ticks(10);
+    const yAxis = d3.axisLeft().scale(y).ticks(10);
 
-    var valueline = d3.line()
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.value); });
+    const valueline = d3.line()
+      .x(d => x(d.date))
+      .y(d => y(d.value));
 
-    x.domain(d3.extent(this.data, function(d) { return d.date; }));
+    x.domain(d3.extent(this.data, d => d.date));
     y.domain([minY, maxY]);
 
     this.canvas.append('path')
@@ -73,10 +71,10 @@ class Chart {
         .data(this.data)
       .enter().append('circle')
         .attr('class', 'dot')
-        .attr('id', function(d, i){ return self.getIdFor(d); })
+        .attr('id', d => this.getIdFor(d))
         .attr('r', this.options.dotRadius)
-        .attr('cx', function(d) { return x(d.date); })
-        .attr('cy', function(d) { return y(d.value); });
+        .attr('cx', d => x(d.date))
+        .attr('cy', d => y(d.value));
 
     // This allows users to not directly hit the dot for it to be selected
     this.canvas.selectAll('.dotMouse')
@@ -85,8 +83,8 @@ class Chart {
         .attr('class', 'dotMouse')
         .attr('r', this.options.dotRadius * 2)
         .attr('opacity', 0)
-        .attr('cx', function(d) { return x(d.date); })
-        .attr('cy', function(d) { return y(d.value); })
+        .attr('cx', d => x(d.date))
+        .attr('cy', d => y(d.value))
         .on('mouseover', this.handleMouseOver())
         .on('mouseout', this.handleMouseOut());
 
@@ -101,38 +99,36 @@ class Chart {
   }
 
   handleMouseOver(){
-    var self = this;
+    return (d, i) => {
+      const week = this.getWeekFor(d);
+      const mins = this.getExtremums(week, 'min');
+      const maxs = this.getExtremums(week, 'max');
 
-    return function(d, i){
-      var week = self.getWeekFor(d);
-      var mins = self.getExtremums(week, 'min');
-      var maxs = self.getExtremums(week, 'max');
-
-      week.forEach(function(day){
+      week.forEach(day => {
         if(mins.includes(day)){
-          self.canvas.select('#' + self.getIdFor(day))
-            .attr('r', self.options.dotBigRadius)
-            .style('fill', self.options.minDotFill);
+          this.canvas.select('#' + this.getIdFor(day))
+            .attr('r', this.options.dotBigRadius)
+            .style('fill', this.options.minDotFill);
         } else if(maxs.includes(day)){
-          self.canvas.select('#' + self.getIdFor(day))
-            .attr('r', self.options.dotBigRadius)
-            .style('fill', self.options.maxDotFill);
+          this.canvas.select('#' + this.getIdFor(day))
+            .attr('r', this.options.dotBigRadius)
+            .style('fill', this.options.maxDotFill);
         }
       });
 
-      self.canvas.append('rect')
+      this.canvas.append('rect')
           .attr('class', 'selectionRectangle')
           .attr('x', 0)
-          .attr('y', -self.options.dotRadius)
-          .attr('width', Math.max(self.getIntAttrFor(week[0], 'cx'), 0)) // Width can't be negative
-          .attr('height', self.options.height + self.options.dotRadius);
+          .attr('y', -this.options.dotRadius)
+          .attr('width', Math.max(this.getIntAttrFor(week[0], 'cx'), 0)) // Width can't be negative
+          .attr('height', this.options.height + this.options.dotRadius);
 
-      self.canvas.append('rect')
+      this.canvas.append('rect')
           .attr('class', 'selectionRectangle')
-          .attr('x', self.getIntAttrFor(week[week.length-1], 'cx'))
-          .attr('y', -self.options.dotRadius)
-          .attr('width', self.options.width - self.getIntAttrFor(week[week.length-1], 'cx'))
-          .attr('height', self.options.height + self.options.dotRadius);
+          .attr('x', this.getIntAttrFor(week[week.length-1], 'cx'))
+          .attr('y', -this.options.dotRadius)
+          .attr('width', this.options.width - this.getIntAttrFor(week[week.length-1], 'cx'))
+          .attr('height', this.options.height + this.options.dotRadius);
     }
   }
 
@@ -141,8 +137,8 @@ class Chart {
   }
 
   getExtremums(data, type){
-    var extremumValue;
-    var cmp;
+    let extremumValue;
+    let cmp;
 
     if(type == 'min'){
       extremumValue = Infinity;
@@ -152,10 +148,9 @@ class Chart {
       cmp = (a, b) => a > b;
     }
 
-    var extremums = [];
-    var self = this;
+    let extremums = [];
 
-    data.forEach(function(day){
+    data.forEach(day => {
       if(cmp(day.value, extremumValue)){
         extremumValue = day.value;
         extremums = [day];
@@ -168,29 +163,24 @@ class Chart {
   }
 
   handleMouseOut(){
-    var self = this;
-
-    return function(d, i){
-      self.clearSelection();
+    return (d, i) => {
+      this.clearSelection();
     }
   }
 
   clearSelection(){
-    var self = this;
+    this.canvas.selectAll('.dot')
+      .style('fill', this.options.defaultDotFill)
+      .attr('r', this.options.dotRadius);
 
-    self.canvas.selectAll('.dot')
-      .style('fill', self.options.defaultDotFill)
-      .attr('r', self.options.dotRadius);
-
-    self.canvas.selectAll('.selectionRectangle').remove();
+    this.canvas.selectAll('.selectionRectangle').remove();
   }
 
   getWeekFor(d){
-    var week = [];
-    var self = this;
+    const week = [];
 
-    this.data.forEach(function(day){
-      if(self.sameWeek(d.date, day.date)){
+    this.data.forEach(day => {
+      if(this.sameWeek(d.date, day.date)){
         week.push(day);
       }
     });
@@ -203,6 +193,6 @@ class Chart {
   }
 
   getIdFor(day){
-    return 'point' + self.name + day.date.toISOString().slice(0,10);
+    return 'point' + this.name + day.date.toISOString().slice(0,10);
   }
 }
